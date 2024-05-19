31f21a9d97cd8da86661c6e723cd4d37f1d291d0
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-05-19 19:22:31 +0530

    Update RecipeController.java

--- src/main/java/com/example/recipe/RecipeController.java
+++ src/main/java/com/example/recipe/RecipeController.java
@@ -1,36 +1,41 @@
 package com.example.recipe;
 
 import java.util.*;
-import org.springframework.web.bind.annotation.*;
 import com.example.recipe.RecipeService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RecipeController {
+
     RecipeService recipeService = new RecipeService();
-    
-    @GetMapping("/recipes") 
+
+    @GetMapping("/recipes")
     public ArrayList<Recipe> getRecipes() {
         return recipeService.getAllRecipes();
 
     }
-    @GetMapping("/recipes/{recipeId)")
+
+    @GetMapping("/recipes/{recipeId}")
     public Recipe getRecipeById(@PathVariable("recipeId") int recipeId) {
         return recipeService.getRecipeById(recipeId);
 
     }
+
     @PostMapping("/recipes")
-    public Recipe addRecipe (@RequestBody Recipe recipe) {
+    public Recipe addRecipe(@RequestBody Recipe recipe) {
         return recipeService.addRecipe(recipe);
     }
+
     @PutMapping("/recipes/{recipeId}")
-    public Recipe updateRecipe (@PathVariable("recipeId") int recipeId, @RequestBody Recipe recipe){
-         return recipeService.updateRecipe(recipeId, recipe);
+    public Recipe updateRecipe(@PathVariable("recipeId") int recipeId, @RequestBody Recipe recipe) {
+        return recipeService.updateRecipe(recipeId, recipe);
 
-}
-@DeleteMapping("/recipes/{recipeId}")
-public void deleteRecipe(@PathVariable("recipeId") int recipeId) { 
-    recipeService.deleteRecipe(recipeId);
+    }
 
-}
+    @DeleteMapping("/recipes/{recipeId}")
+    public void deleteRecipe(@PathVariable("recipeId") int recipeId) {
+        recipeService.deleteRecipe(recipeId);
+
+    }
 
 }
